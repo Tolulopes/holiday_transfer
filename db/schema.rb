b5bf7b3402ee62bf63b3f9119265d6444a84ccf9
Tolu Shobande <Tolu.Shobande@hotmail.com>
2015-03-24 18:20:54 +0000

    added attractions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323181934) do
+ActiveRecord::Schema.define(version: 20150324181849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attractions", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "location"
+    t.datetime "date"
+    t.integer  "package_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "packages", force: :cascade do |t|
     t.integer  "user_id"
