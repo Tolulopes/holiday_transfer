6995fdb087f1deefb98d07aa258bc01c5c87c4e3
Tolu Shobande <Tolu.Shobande@hotmail.com>
2015-03-25 20:08:03 +0000

    added gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325110727) do
+ActiveRecord::Schema.define(version: 20150325200226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20150325110727) do
     t.integer  "package_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.float    "Longitude"
+    t.float    "Latitude"
   end
 
   create_table "destinations", force: :cascade do |t|
