c70692ef1fee40758990ae37af53d8b411716fbf
Tolu Shobande <Tolu.Shobande@hotmail.com>
2015-03-25 23:30:32 +0000

    updated packages show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,14 @@ Hotel.delete_all
 Destination.delete_all
 
 u1 = User.create(first_name: 'Tolu', email: 'Tolu@me.com', password: 'password', password_confirmation: 'password')
+
 u2 = User.create(first_name: 'David', email: 'David@me.com', password: 'password', password_confirmation: 'password')
 
 d1 = Destination.create(name: 'Dubai')
 d2 = Destination.create(name: 'BoraBora')
 
 p1 = Package.create(user_id: u1.id , description: 'This includes flights hotels and 3 pre-booked attractions', price: 2000)
+
 p2 = Package.create(user_id: u2.id, description: 'Five Star vacation with two first class tickets to bora bora', price: 5000)
 
 
@@ -27,10 +29,10 @@ t1 = Ticket.create(current_location: 'London', destination_id: d1.id, ticket_cla
 t2 = Ticket.create(current_location: 'London', destination_id: d2.id, ticket_class: 'First' , airline: 'British Airways', booking_reference: 'ba986', baggage_allowance: '23', departure_time: '2015-05-29-09:00', arrival_time: '2015-05-30 10:00', round_trip: true, price: 3500, package_id: p2.id)
 
 a1 = Attraction.create(name: 'Desert Excursion' , description: 'Camel Rides & Quadbiking' , location: 'Golden Desert Dubai' , date: '2015-04-22-14:00', package_id: p1.id)
-a3 = Attraction.create(name: 'Palm Hotel' , description: 'Fancy Lunch' , location: 'palm beach dubai' , date: '2015-04-24-14:00', package_id: p1.id)
-
 
 a2 = Attraction.create(name: 'Bora Bora Dream' , description: 'Love Boat Ride' , location: 'Tourquise Lagoon' , date: '2015-05-30-13:00', package_id: p2.id)
+
+a3 = Attraction.create(name: 'Palm Hotel' , description: 'Fancy Lunch' , location: 'palm beach dubai' , date: '2015-04-24-14:00', package_id: p1.id)
 
 h1 = Hotel.create(name: 'Jumeirah Beach Hotel' , address: 'Jumeirah Beach Hotel Jumeirah Dubai United Arab Emirates' , checkin: '2015-04-22-15:00', checkout: '2014-04-30-12:00' , stars: '5' , package_id: p1.id)
 
